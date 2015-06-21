fd70563a3537a2df3c8071b727971ced628f3e65
Sato Makoto <makoto@kuzuore.com>
2015-06-21 09:33:28 +0900

    clear directory/files again

--- my8x8_half_on_off/my8x8_half_on_off.ino
+++ my8x8_half_on_off/my8x8_half_on_off.ino
@@ -15,18 +15,38 @@ void setup ()
 }
 
 static const int all_on = B11111111;
+static const int half_on1 = B10101010;
+static const int half_on2 = B01010101;
+
 static const uint8_t PROGMEM 
-  every_bmp[] =
-  { all_on, all_on, all_on, all_on,
-    all_on, all_on, all_on, all_on };
+
+// every_bmp1 light like:
+// O _ O _ O _ O _
+// _ O _ O _ O _ O
+// O _ O _ O _ O _
+// O _ O _ O _ O _
+// _ O _ O _ O _ O
+// _ O _ O _ O _ O
+// O O O O O O O O
+// O _ O _ O _ O _
+
+  every_bmp1[] =
+  { half_on1, half_on2, half_on1, half_on1,
+    half_on2, half_on2, all_on, half_on1},
+
+  every_bmp2[] =
+  { half_on2, half_on1, half_on2, half_on1,
+    half_on2, half_on1, half_on2, half_on1 };
    
 void loop ()
 {
   matrix.clear ();
-  matrix.drawBitmap (0, 0, every_bmp, 8, 8, LED_ON);
+  matrix.drawBitmap (0, 0, every_bmp1, 8, 8, LED_ON);
   matrix.writeDisplay ();
-  delay (500);
-  matrix.drawBitmap (0, 0, every_bmp, 8, 8, LED_OFF);
+  delay (1500);
+
+  matrix.clear();
+  matrix.drawBitmap (0, 0, every_bmp2, 8, 8, LED_ON);
   matrix.writeDisplay ();
   delay (500);
 }
